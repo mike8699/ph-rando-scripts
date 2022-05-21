419a8964d41f6c4563e20558d1aa77bb1da20f46
Mike <91105575+mikeoss@users.noreply.github.com>
2022-05-21 14:46:48 -0400

    Support searching by item name in addition to id

--- search_zmb.py
+++ search_zmb.py
@@ -6,8 +6,10 @@ from ndspy import lz10
 from zed.zmb import ZMB
 from zed.common import Game
 
+from patcher.items import ITEMS
 
-def search_zmb(rompath: str, item_ids: list[int]):
+
+def search_zmb(rompath: str, item_ids: list[int | str]):
     """
     Find a ZMB that contains the given items.
 
@@ -15,6 +17,10 @@ def search_zmb(rompath: str, item_ids: list[int]):
         rompath: path to the ROM
         item_ids: list of item ids to search ZMBs for
     """
+    for i, item_id in enumerate(item_ids):
+        if isinstance(item_id, str):
+            item_ids[i] = ITEMS[item_id]
+
     rom = NintendoDSRom.fromFile(rompath)
     locations = []
     for folder in rom.filenames.subfolder('Map').folders:
@@ -31,8 +37,8 @@ def search_zmb(rompath: str, item_ids: list[int]):
                     zmb_filename = f'zmb/{folder_name}_{map_number}.zmb'
                     try:
                         zmb_file = ZMB(Game.PhantomHourglass, narc_file.getFileByName(f'zmb/{folder_name}_{map_number}.zmb'))
-                        searching_for = [item for item in item_ids] # deep clone the original list
-                        for map_obj in zmb_file.mapObjects:
+                        searching_for = [item for item in item_ids]  # deep clone the original list
+                        for i, map_obj in enumerate(zmb_file.mapObjects):
                             if map_obj.unk08 in searching_for:
                                 searching_for.pop(searching_for.index(map_obj.unk08))
                         if len(searching_for) == 0:
@@ -43,5 +49,5 @@ def search_zmb(rompath: str, item_ids: list[int]):
     return locations
 
 
-locations = search_zmb('in_dpad.nds', [0x2d])
+locations = search_zmb('in_dpad.nds', ["small_key", "small_key", "small_red_rupee"])
 pprint(locations)
